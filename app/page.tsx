dfdf97b32416f6eb4437904de115ef254edafd65
Guilherme Gervasio <guilherme.silva0509@outlook.com>
2025-02-18 15:59:35 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -27,6 +30,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-bold">Corte de Cabelo</h3>
+
+              <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">18</p>
+              <p className="text-sm">15:56</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
